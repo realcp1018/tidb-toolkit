b6382305aabc25e56f967eba85ca3b6feb474db6
realcp1018 <forevermessi@foxmail.com>
2023-02-28 12:40:37 +0800

    fix space used ratio display precision

--- utils/bytesize.py
+++ utils/bytesize.py
@@ -12,10 +12,10 @@ Usage:
 
 class ByteSize(object):
     BYTE_SIZE_MAP = {
-        "KiB": 2 ** 10,
-        "MiB": 2 ** 20,
-        "GiB": 2 ** 30,
-        "TiB": 2 ** 40
+        "KiB": 1 << 10,
+        "MiB": 1 << 20,
+        "GiB": 1 << 30,
+        "TiB": 1 << 40
     }
 
     def __init__(self, size: str):
